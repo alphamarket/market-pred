c6c90b73b8ff7512515516285fbd460cb764963b
noise2 <b.g.dariush@gmail.com>
2019-03-05 14:20:48 +0330

    @ create_trainset.m: huge fixes

--- create_trainset.m
+++ create_trainset.m
@@ -8,31 +8,27 @@ elseif(exist('caches/trainset.dat', 'file'))
 else
   fprintf('Creating the trainset')
   lastsize = 0;
-  trainset.m1 = { };
-  trainset.m3 = { };
-  trainset.m7 = { };
-  trainset.m14 = { };
-  trainset.m28 = { };
-  trainset.m56 = { };
+  trainset = { };
+  map = [1, 3, 7, 14, 28, 56];
   for set=1:length(gainset)
     gset = gainset{set};
     if(size(gset, 1) < 57)
       continue
     end
+    trainset{end + 1} = struct;
+    for m = 1:length(map)
+      eval(sprintf('trainset{end}.m%i = { };', map(m)));
+    end
     for col=1:size(gset, 2)
-      trainset.m1{end + 1} = [];
-      trainset.m3{end + 1} = [];
-      trainset.m7{end + 1} = [];
-      trainset.m14{end + 1} = [];
-      trainset.m28{end + 1} = [];
-      trainset.m56{end + 1} = [];
+      fprintf(repmat('\b', 1, lastsize))
+      lastsize = fprintf(' — %.1f%% of data is getting processed. [step %i of %i]', set / length(gainset) * 100, col, size(gset, 2)); 
+      for m = 1:length(map)
+        eval(sprintf('trainset{end}.m%i{end + 1} = [];', map(m)));
+      end
       for idx=57:length(gset)
-        trainset.m1{end} = [trainset.m1{end}; gset(idx - 1, col), gset(idx, col)];
-        trainset.m3{end} = [trainset.m3{end}; gset(idx - 3:idx - 1, col)', gset(idx, col)];
-        trainset.m7{end} = [trainset.m7{end}; gset(idx - 7:idx - 1, col)', gset(idx, col)];
-        trainset.m14{end} = [trainset.m14{end}; gset(idx - 14:idx - 1, col)', gset(idx, col)];
-        trainset.m28{end} = [trainset.m28{end}; gset(idx - 28:idx - 1, col)', gset(idx, col)];
-        trainset.m56{end} = [trainset.m56{end}; gset(idx - 56:idx - 1, col)', gset(idx, col)];
+        for m = 1:length(map)
+          eval(sprintf('trainset{end}.m%i{end} = [trainset{end}.m%i{end}; gset(idx - %i:idx - 1, col)'', gset(idx, col)];', map(m), map(m), map(m)));
+        end
       end
     end
     fprintf(repmat('\b', 1, lastsize));
@@ -41,5 +37,5 @@ else
   save('caches/trainset.dat', 'trainset', '-v7.3')
   fprintf(repmat('\b', 1, lastsize));
   fprintf(' [DONE]\n');
-  clear set idx lastsize gset col
+  clear set idx lastsize gset col map m
 end
--- main.m
+++ main.m
@@ -2,3 +2,4 @@ close all; clc;
 load_dataset;
 preprocess_dataset;
 create_trainset;
+% train_module = train_dataset(trainset);
